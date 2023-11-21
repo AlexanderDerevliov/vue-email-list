f506a3dace0a9fe02a17081f104b6b923d86c2c4
AlexanderDerevliov <alex.feelred@gmail.com>
2023-11-21 22:41:25 +0100

    ended exercise

--- js/script.js
+++ js/script.js
@@ -5,8 +5,32 @@ createApp ({
     data: () => {
         return {
             eMail: [],
-            eMailNumber: null,
+            emailNumber: null,
             error: '',      
         }
     },
-})
+
+    computed: {
+        endEmailArray () {
+            return this.eMail.length === this.emailNumber 
+        }
+    },
+
+    methods: {
+        getRandomEmail () {
+            this.eMail = [];
+            for(let i = 0; i < this.emailNumber; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((res) => {
+                        if(this.eMail.includes(res.data.response)){
+                            this.emailNumber++
+                        } else {
+                            const randomEmail = res.data.response
+                            this.eMail.push(randomEmail)
+                        }
+                    })
+            }
+        }
+    },
+
+}) .mount('#app')
